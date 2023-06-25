a08963d1e6f8995ae40b2c21b1fdd85f53429053
unknown <jjatuah@gmail.com>
2023-06-25 17:08:54 +0100

    80% done with home page

--- scissor_url_shortener_frontend/src/pages/home/Home.jsx
+++ scissor_url_shortener_frontend/src/pages/home/Home.jsx
@@ -2,9 +2,27 @@ import "./Home.css";
 import Nav from "../../components/nav/Nav";
 
 const Home = () => {
+
+  const text = "Short URL that will be generated"
   return ( 
     <div className="home">
       <Nav />
+      <div className="inputs">
+        <form>
+          <p>Let's help you cut that URL short</p>
+          <label>Long URL</label>
+          <input type="url" placeholder="Enter Long URL" required/>
+          <label>Shortcode For Customization</label>
+          <input type="text" placeholder="Enter Your own shortcode if you feel like"/>
+          <button>Shorten</button>
+        </form>
+      </div>
+
+      <div className="outputs">
+        <p>{text}</p>
+        <button  onClick={() => {
+          navigator.clipboard.writeText(text);}}>Click to copy short URL</button>
+      </div>
     </div>
    );
 }

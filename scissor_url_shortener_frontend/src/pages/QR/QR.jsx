61f1839b13393d248d060c05a97802af641ac92a
unknown <jjatuah@gmail.com>
2023-06-25 23:06:38 +0100

    80% done with QR Code page

--- scissor_url_shortener_frontend/src/pages/QR/QR.jsx
+++ scissor_url_shortener_frontend/src/pages/QR/QR.jsx
@@ -6,7 +6,18 @@ const QR = () => {
   return ( 
     <div className="qr">
       <Nav />
-      <h1>QR Code</h1>
+      <div className="qrInfo">
+        <h2>Links</h2>
+        <div className="link">
+          <p>www.Google.com</p>
+          <p>Generate QR Code</p>
+        </div>
+
+        <div className="link">
+          <p>https://www.hotnigerianjobs.com/field/236/web-developer-jobs-in-nigeria</p>
+          <p>Generate QR Code</p>
+        </div>
+      </div>
     </div>
    );
 }

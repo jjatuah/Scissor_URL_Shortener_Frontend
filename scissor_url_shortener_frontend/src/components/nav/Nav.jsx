248e78ee905ec5159466d8d7c0fae137c969c6d5
unknown <jjatuah@gmail.com>
2023-06-25 16:17:53 +0100

    completed navbar

--- scissor_url_shortener_frontend/src/components/nav/Nav.jsx
+++ scissor_url_shortener_frontend/src/components/nav/Nav.jsx
@@ -4,7 +4,15 @@ import "./Nav.css"
 const Nav = () => {
   return ( 
     <div className="nav">
-      
+      <div className="header">
+        <h1>SCISSOR</h1>
+      </div>
+
+      <div className="navLinks">
+        <a href="#">Create Link</a>
+        <a href="#">QR Code</a>
+        <a href="#">My Links</a>
+      </div>
     </div>
    );
 }
--- scissor_url_shortener_frontend/src/pages/home/Home.jsx
+++ scissor_url_shortener_frontend/src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import "./Home.css";
+import Nav from "../../components/nav/Nav";
 
 const Home = () => {
   return ( 
     <div className="home">
-      
+      <Nav />
     </div>
    );
 }

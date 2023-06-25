73f8303160437cfc9dd60fde1a7fb6bf4137c163
unknown <jjatuah@gmail.com>
2023-06-26 00:31:14 +0100

    completed pop up functionality and QR Code page

--- scissor_url_shortener_frontend/src/components/pop_up/Pop.jsx
+++ scissor_url_shortener_frontend/src/components/pop_up/Pop.jsx
@@ -1,12 +1,16 @@
 import "./PopUp.css"
+import testImg from "../../assets/bg1.jpg"
 
-
-const PopUp = () => {
-  return ( 
+const PopUp = (props) => {
+  return (props.trigger) ? ( 
     <div className="popUp">
-      
+      <div className="popUpInfo">
+        <p onClick={() => props.setTrigger(false)}>Click to close</p>
+        <img src={testImg} alt="" />
+        {props.trigger}
+      </div>
     </div>
-   );
+   ) : "";
 }
  
 export default PopUp;
--- scissor_url_shortener_frontend/src/pages/QR/QR.jsx
+++ scissor_url_shortener_frontend/src/pages/QR/QR.jsx
@@ -1,8 +1,12 @@
 import "./QR.css";
 import Nav from "../../components/nav/Nav";
+import PopUp from "../../components/pop_up/Pop";
+import { useState } from "react";
 
 
 const QR = () => {
+
+  const [buttonPopUp, setButtonPopUp] = useState(false)
   return ( 
     <div className="qr">
       <Nav />
@@ -10,13 +14,14 @@ const QR = () => {
         <h2>Links</h2>
         <div className="link">
           <p>www.Google.com</p>
-          <p>Generate QR Code</p>
+          <p onClick={() => setButtonPopUp(true)}>Generate QR Code</p>
         </div>
 
         <div className="link">
           <p>https://www.hotnigerianjobs.com/field/236/web-developer-jobs-in-nigeria</p>
-          <p>Generate QR Code</p>
+          <p onClick={() => setButtonPopUp(true)}>Generate QR Code</p>
         </div>
+        <PopUp setTrigger={setButtonPopUp} trigger={buttonPopUp} />
       </div>
     </div>
    );

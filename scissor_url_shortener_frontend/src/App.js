4afe442cdca58efc640b2b69133f8a38333f8912
unknown <jjatuah@gmail.com>
2023-06-25 22:12:47 +0100

    Started working on the QR code Page

--- scissor_url_shortener_frontend/src/App.js
+++ scissor_url_shortener_frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/login/Login';
 import Home from './pages/home/Home';
 import MyLinks from './pages/myLinks/MyLinks';
 import Nav from './components/nav/Nav';
+import QR from './pages/QR/QR';
 
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       {/* <Register />
       <Login /> */}
       {/* <Home /> */}
-      <MyLinks />
+      {/* <MyLinks /> */}
+      <QR />
     </div>
   );
 }
---
+++ scissor_url_shortener_frontend/src/pages/QR/QR.jsx
@@ -0,0 +1,14 @@
+import "./QR.css";
+import Nav from "../../components/nav/Nav";
+
+
+const QR = () => {
+  return ( 
+    <div className="qr">
+      <Nav />
+      <h1>QR Code</h1>
+    </div>
+   );
+}
+ 
+export default QR;

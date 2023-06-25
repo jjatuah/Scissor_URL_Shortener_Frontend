80a4f1cbdd7efc62cc3ad8bce45d731dcc47b0d5
unknown <jjatuah@gmail.com>
2023-06-25 16:01:01 +0100

    started working on navbar

--- scissor_url_shortener_frontend/src/App.js
+++ scissor_url_shortener_frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Register from './pages/register/Register';
 import Login from './pages/login/Login';
+import Home from './pages/home/Home';
+
 
 function App() {
   return (
     <div className="App">
-      <Register />
-      <Login />
+      {/* <Register />
+      <Login /> */}
+      <Home />
     </div>
   );
 }

0be2b695f5dcb010dba2fbd7fd09fbaba308d005
unknown <jjatuah@gmail.com>
2023-07-02 15:03:17 +0100

    completed register route

--- scissor_url_shortener_backend/routes/register.route.js
+++ scissor_url_shortener_backend/routes/register.route.js
@@ -1,2 +1,25 @@
 const express = require("express");
+const userModel = require("../models/users.model");
 require('dotenv').config();
+
+const registerRoute = express.Router();
+
+registerRoute.post("/", async (req, res) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const newUser = new userModel.create({
+      email : email,
+      password : password
+    })
+
+    console.log(newUser);
+    res.status(200).send("Registered")
+  } catch (error) {
+    res.status(400)
+    console.log(error).send(error);
+  }
+})
+
+module.exports = registerRoute; 
